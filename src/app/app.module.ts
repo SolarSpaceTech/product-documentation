d5c4cd4e94d213d53817e27bdad2bdd1a0e3f8f6
Maksim Voropay <maksim.voropay@mvideo.ru>
2024-09-10 13:47:20 +0300

    removed useless code form app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from "@angular/common/http";
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
