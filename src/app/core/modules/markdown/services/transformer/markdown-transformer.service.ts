d7baba508e5073cf861db6522d7c21c98990abfd
Maksim Voropay <maksim.voropay@mvideo.ru>
2024-09-10 14:20:12 +0300

    updated markdown transformer service

--- src/app/core/modules/markdown/services/transformer/markdown-transformer.service.ts
+++ src/app/core/modules/markdown/services/transformer/markdown-transformer.service.ts
@@ -1,42 +1,19 @@
 import { Injectable } from "@angular/core";
 import { marked, Token } from 'marked';
-import { MarkdownTokenModel } from "../../models";
-import { MarkdownInlineEnum } from "../../enums";
+import { MarkdownParserService } from '../parser/markdown-parser.service';
 
 @Injectable()
 export class MarkdownTransformerService {
-  private readonly TARGET_LINK_RULE = RegExp(marked.Lexer.rules.inline.normal.link.source.replace('^!?', '^{:target=_blank}'));
+  constructor(private readonly markdownParserService: MarkdownParserService) {}
 
-  public render(tokens: Token[]): string {
-    return marked.parser(tokens);
+  public render(tokens: Token[], inline = false): string {
+    if (inline) {
+      return this.markdownParserService.parseInline(tokens);
+    }
+    return this.markdownParserService.parse(tokens);
   }
 
   public transform(content: string): Token[] {
-    this.addTargetLinkToParser();
     return marked.lexer(content);
-  }
-
-  private addTargetLinkToParser(): void {
-    marked.use({
-      extensions: [{
-        name: 'targetLink',
-        level: 'inline',
-        start: (src: string): number => src.match(/^{:target=_blank}/)?.index,
-        tokenizer: (src: string): any => {
-          const cap = this.TARGET_LINK_RULE.exec(src);
-          if (cap) {
-            return {
-              type: MarkdownInlineEnum.TargetLink,
-              raw: cap[0],
-              href: cap[2],
-              text: cap[1],
-            };
-          }
-        },
-        renderer(token: MarkdownTokenModel): string {
-          return `<a href="${token.href}" target="_blank">${token.text}</a>`;
-        }
-      }],
-    });
   }
 }
