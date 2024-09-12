ee288b79775a6ef2cc041e6c2eb460f32a5bab75
Maksim Voropay <maksim.voropay@mvideo.ru>
2024-09-12 13:33:27 +0300

    created MarkdownTableComponent

--- src/app/core/modules/markdown/components/block/index.ts
+++ src/app/core/modules/markdown/components/block/index.ts
@@ -2,3 +2,4 @@ export * from './blockquote/markdown-blockquote.component';
 export * from './code/markdown-code.component';
 export * from './heading/markdown-heading.component';
 export * from './paragraph/markdown-paragraph.component';
+export * from './table/markdown-table.component';
