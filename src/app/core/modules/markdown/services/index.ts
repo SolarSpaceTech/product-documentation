6abccb0a6e26d275a92100ac3dfd004871093434
Maksim Voropay <maksim.voropay@mvideo.ru>
2024-09-12 13:40:48 +0300

    updated index.ts

--- src/app/core/modules/markdown/services/index.ts
+++ src/app/core/modules/markdown/services/index.ts
@@ -1,2 +1,3 @@
 export { MarkdownComponentCreatorService } from './component-creator/markdown-component-creator.service';
 export { MarkdownTransformerService } from './transformer/markdown-transformer.service';
+export { MarkdownRendererService } from './renderer/markdown-renderer.service';
