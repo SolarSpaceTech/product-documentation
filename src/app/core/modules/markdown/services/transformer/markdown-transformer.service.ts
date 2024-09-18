c7e390f64c7f7e509f53b2c4c8484fd0c1c669a5
Maksim Voropay <maksim.voropay@mvideo.ru>
2024-09-18 13:41:45 +0300

    refactor

--- src/app/core/modules/markdown/services/transformer/markdown-transformer.service.ts
+++ src/app/core/modules/markdown/services/transformer/markdown-transformer.service.ts
@@ -1,18 +1,8 @@
 import { Injectable } from "@angular/core";
 import { marked, Token } from 'marked';
-import { MarkdownParserService } from '../parser/markdown-parser.service';
 
 @Injectable()
 export class MarkdownTransformerService {
-  constructor(private readonly markdownParserService: MarkdownParserService) {}
-
-  public render(tokens: Token[], inline = false): string {
-    if (inline) {
-      return this.markdownParserService.parseInline(tokens);
-    }
-    return this.markdownParserService.parse(tokens);
-  }
-
   public transform(content: string): Token[] {
     return marked.lexer(content);
   }
