8ce8a4087965f30aee45b72a90c4a2bd740f6516
Alan Ageev <alan.ageev@maximumtest.ru>
2024-09-18 13:30:45 +0300

    SCPL-668: Хедер и футер - стили

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,10 @@
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Inject,
-  Input,
-  PLATFORM_ID,
-} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SCInputModule } from '@ui-kit/input/input.module';
 import { SCDecoratorsModule } from '@ui-kit/decorators/decorators.module';
 import { SCIconModule } from '@ui-kit/icon/icon.module';
 import { SCCheckboxModule } from '@ui-kit/checkbox/checkbox.module';
+import { SCSelectModule } from '@ui-kit/select/select.module';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -22,6 +17,7 @@ import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
     SCInputModule,
     SCIconModule,
     SCCheckboxModule,
+    SCSelectModule,
     ReactiveFormsModule,
     FormsModule,
   ],
