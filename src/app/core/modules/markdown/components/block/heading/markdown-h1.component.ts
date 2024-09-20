bcf1e7b15dbfd20908a03d9546a47054a1f15070
Maksim Voropay <maksim.voropay@mvideo.ru>
2024-09-20 10:35:19 +0300

    remove useless code from heading

--- src/app/core/modules/markdown/components/block/heading/markdown-h1.component.ts
+++ src/app/core/modules/markdown/components/block/heading/markdown-h1.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 
 import { MarkdownHeadingDirective } from './markdown-heading.directive';
-import { MarkdownContentDirective } from 'markdown/diretives';
 
 @Component({
   selector: 'h1.markdown-heading',
   template: ``,
   standalone: true,
-  imports: [
-    MarkdownContentDirective
-  ],
 })
 export class MarkdownH1Component extends MarkdownHeadingDirective {}
--- src/app/core/modules/markdown/components/block/heading/markdown-h5.component.ts
+++ src/app/core/modules/markdown/components/block/heading/markdown-h5.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 
 import { MarkdownHeadingDirective } from './markdown-heading.directive';
-import { MarkdownContentDirective } from 'markdown/diretives';
 
 @Component({
   selector: 'h5.markdown-heading',
   template: ``,
   standalone: true,
-  imports: [
-    MarkdownContentDirective
-  ],
 })
 export class MarkdownH5Component extends MarkdownHeadingDirective {}
